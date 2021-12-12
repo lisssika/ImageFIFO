635eae53ee76ade8fb324942092740f6eb7c0f0b
Таня Сайкина <posmotri.vokrug777@gmail.com>
2021-12-12 17:41:11 +0700

    синхронные и асинхронные тесты работают!

--- ImageFIFO/ReadAndWrite.cpp
+++ ImageFIFO/ReadAndWrite.cpp
@@ -12,27 +12,26 @@ int writer(ImageFIFO& fifo, const std::string& file_names)
 	std::stringstream file_names_(file_names);
 	std::ifstream file;
 	void* data;
-	for (std::string file_name; std::getline(file_names_, file_name, ' '); ) {
+	for (std::string file_name; std::getline(file_names_, file_name, ' ');)
+	{
 		data = fifo.getFree();
-		if (data)
-		{
-			file.open(file_name, std::ios::binary);
-			if (file.is_open())
-			{
-				file.read(static_cast<char*>(data), fifo.get_blockSize());
-				fifo.addReady(data);
-				ready_blocks_added.notify_all();
-				file.close();
-			}
-			else
-			{
-				throw std::runtime_error("file can't open");
-			}
-		}
-		else
+		if (!data)
 		{
 			std::unique_lock<std::mutex> lk(mutex_);
 			free_blocks_added.wait(lk);
+			data = fifo.getFree();
+		}
+		file.open(file_name, std::ios::binary);
+		if (file.is_open())
+		{
+			file.read(static_cast<char*>(data), fifo.get_blockSize());
+			fifo.addReady(data);
+			ready_blocks_added.notify_all();
+			file.close();
+		}
+		else
+		{
+			throw std::runtime_error("file can't open");
 		}
 	}
 	return 0;
@@ -43,27 +42,27 @@ int reader(ImageFIFO& fifo, const std::string& file_names)
 	std::stringstream files(file_names);
 	std::ofstream file;
 	void* data;
-	for (std::string file_name; std::getline(files, file_name, ' '); ) {
+	for (std::string file_name; std::getline(files, file_name, ' ');)
+	{
 		data = fifo.getReady();
-		if (data)
-		{
-			file.open(file_name, std::ios::binary);
-			if (file.is_open())
-			{
-				file.write(static_cast<char*>(data), fifo.get_blockSize());
-				fifo.addFree(data);
-				free_blocks_added.notify_all(); // noify_one???
-				file.close();
-			}
-			else
-			{
-				throw std::runtime_error("file can't open");
-			}
-		}
-		else
+		if (!data)
 		{
 			std::unique_lock<std::mutex> lk(mutex_);
 			ready_blocks_added.wait(lk);
+			data = fifo.getReady();
+		}
+
+		file.open(file_name, std::ios::binary);
+		if (file.is_open())
+		{
+			file.write(static_cast<char*>(data), fifo.get_blockSize());
+			fifo.addFree(data);
+			free_blocks_added.notify_all(); // noify_one???
+			file.close();
+		}
+		else
+		{
+			throw std::runtime_error("file can't open");
 		}
 	}
 	return 0;
